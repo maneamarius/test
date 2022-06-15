c4cec13dc30ffc73a8f8ba13777a91074c26abf6
Marius Manea <locomotion.itservices@gmail.com>
2022-06-15 15:31:42 +0300

    add release workflow

---
+++ .github/scripts/prepare-release-base.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+##############################################################
+# Core logic
+
+echo "Not running!"
+echo "::set-output name=run::false"
+echo "::set-output name=type::nightly"
+
+
+#echo "::set-output name=run::true"
